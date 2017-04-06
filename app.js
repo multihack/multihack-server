30b5682b242086b1966817bf81b933ff3e89428a
RationalCoding <thomasmullendesign@gmail.com>
2017-04-06 08:12:45 -0400

    project resync

--- app.js
+++ app.js
@@ -12,33 +12,71 @@ app.get('/', function (req, res, next) {
 })
 
 var calls = {}
+var rooms = {}
 
 io.on('connection', function (socket) {  
   socket.on('join', function (data) {
     if (!data.room) return
+    
     socket.join(data.room)
     socket.room = data.room
+    
+    rooms[socket.room] = rooms[socket.room] || []
+    rooms[socket.room].push(socket.id)
+    
+    socket.emit('join')
   })
   
   socket.on('forward', function (data) {
+    if (!socket.room) return
+    
     socket.broadcast.to(socket.room).emit('forward', data)
   })
   
+  socket.on('provideFile', function (data) {
+    if (!socket.room) return
+    if (!data.filePath || data.content === undefined || !data.requester) return
+    
+    socket.broadcast.to(data.requester).emit('provideFile', data)
+  })
+  
+  socket.on('requestProject', function () {
+    if (!socket.room) return  
+    
+    var provider = rooms[socket.room][0]
+    if (provider === socket.id) provider = rooms[socket.room][1] // is first peer
+    if (!provider) return // is only peer
+    
+    socket.broadcast.to(provider).emit('requestProject', {
+      requester: socket.id
+    })
+  })
+  
   socket.on('voice-join', function () {
+    if (!socket.room) return
+    
     calls[socket.room] = calls[socket.room] || []
     calls[socket.room].push(socket.id)
   })
   
   socket.on('voice-leave', function () {
+    if (!socket.room) return
+    
     calls[socket.room] = calls[socket.room] || []
     var index = calls[socket.room].indexOf(socket.id)
-    calls[socket.room].splice(index,1)
+    calls[socket.room].splice(index, 1)
   })
   
   socket.on('disconnect', function () {
+    if (!socket.room) return
+    
     calls[socket.room] = calls[socket.room] || []
     var index = calls[socket.room].indexOf(socket.id)
-    calls[socket.room].splice(index,1)
+    calls[socket.room].splice(index, 1)
+    
+    rooms[socket.room] = rooms[socket.room] || []
+    index = rooms[socket.room].indexOf(socket.id)
+    rooms[socket.room].splice(index, 1) 
   })
 })
 
